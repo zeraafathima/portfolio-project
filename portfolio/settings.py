e45126d698f98ac0f71b69cd1376f90311c0538d
zeraafathima <fathimadilawar09@gmail.com>
2025-03-01 12:43:58 +0530

    Fixed static file serving

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'myapp'
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
